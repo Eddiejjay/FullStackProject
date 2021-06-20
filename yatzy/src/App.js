f29d745f31d8c4ad354fb63366aeeff9ed620a4f
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-06-20 22:45:38 +0300

    Nopat lisätty

--- yatzy/src/App.js
+++ yatzy/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import YatzyTable from './components/YatzyTable'
 import { useDispatch } from 'react-redux'
 import { initializePoints }  from './reducers/pointsReducer'
+import Dices from './components/Dices'
 
 
 
@@ -20,6 +21,7 @@ const App = () => {
   return (
     <div>
       <h1>Yatzy</h1>
+      <Dices></Dices>
       <YatzyTable></YatzyTable>
     </div>
   )
---
+++ yatzy/src/components/Dices.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDice from 'react-dice-complete'
+import 'react-dice-complete/dist/react-dice-complete.css'
+
+
+const Dices = () => {
+
+
+
+
+  return (
+
+    <div>
+      <ReactDice
+        numDice={5} />
+    </div>
+  )
+}
+
+
+export default Dices
